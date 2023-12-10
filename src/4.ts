13afeebce836e47cf98be097d678df1d5cf3d0c9
Yuriy-St <yuriystaynov@gmail.com>
2023-12-11 01:04:11 +0200

    done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,54 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  private tenants: Person[] = [];
+  public door: boolean = false;
+  public key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  public openDoor(key: Key): void {
+    if (this.key === key) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +57,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
